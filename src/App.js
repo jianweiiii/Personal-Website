19cd8df8f5b17dbb001fa7493aec9f49707246f0
jianweiiii <jianweii97@hotmail.com>
2025-04-29 14:25:08 +0800

    test commit for vercel deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-400 to-pink-500">
-      <h1 className="text-5xl font-bold text-white">Hello, Portfolio!</h1>
+      <h1 className="text-5xl font-bold text-white">Testing deployment to vercel!</h1>
     </div>
   );
 }
